ef19c976a53021ddab8499a9871d73d56805275d
oryna <ec20251@qmul.ac.uk>
2024-01-22 21:41:52 +0000

    more ui changes

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -11,62 +11,67 @@ function Navbar() {
           <label htmlFor="my-drawer" className="">
             <h4>
               <FontAwesomeIcon
-                className="text-3xl p-5 color-red-500"
+                className="text-2xl py-12 px-12"
                 icon={icon({ name: "list" })}
               />
             </h4>
           </label>
         </div>
-        <div className="drawer-side pt-[5rem]">
+        <div className="drawer-side pt-[5rem]  z-[-1]">
           <label
             htmlFor="my-drawer"
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
           <div className="menu p-4 w-2/5 min-h-full bg-base-content text-white">
-            {/* Sidebar content here */}
+            <div>
+              <div>
+                <div className="">
+                  <FontAwesomeIcon
+                    className="text-xl p-4 text-slate-400 z-10"
+                    s
+                    icon={icon({ name: "search" })}
+                  />
+                </div>
+              </div>
+              <div className=" mb-4 rounded-md  p-9 mx-9 bg-slate-700 text-slate-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                minima vero officiis ratione nam, provident ab dignissimos et
+                minus necessitatibus! Accusamus voluptatum in neque, eum est,
+                sunt, et fugiat ullam ducimus cumque unde voluptas consequatur!
+                ccusamus voluptatum in neque, eum est, sunt, et fugiat ullam
+                ducimus cumque unde voluptas consequatur! ccusamus voluptatum in
+                neque, eum est,
+              </div>
+            </div>
 
             <div>
-              <FontAwesomeIcon
-                className="text-2xl p-4"
-                s
-                icon={icon({ name: "robot" })}
-              />
-            </div>
-            <div className="border border-indigo-500 bg-slate-800 rounded-md p-6 mx-9">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-              minima vero officiis ratione nam, provident ab dignissimos et
-              minus necessitatibus! Accusamus voluptatum in neque, eum est,
-              sunt, et fugiat ullam ducimus cumque unde voluptas consequatur!
-            </div>
-
-            <div className=" mt-10 rounded-md  p-9 mx-9 bg-slate-700 text-slate-300">
-              <FontAwesomeIcon
-                className="pr-2 text-md"
-                icon={icon({ name: "search" })}
-              />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-              minima vero officiis ratione nam, provident ab dignissimos et
-              minus necessitatibus! Accusamus voluptatum in neque, eum est,
-              sunt, et fugiat ullam ducimus cumque unde voluptas consequatur!
-              ccusamus voluptatum in neque, eum est, sunt, et fugiat ullam
-              ducimus cumque unde voluptas consequatur! ccusamus voluptatum in
-              neque, eum est,
+              <div>
+                <FontAwesomeIcon
+                  className="text-2xl p-4 text-slate-400"
+                  s
+                  icon={icon({ name: "robot" })}
+                />
+              </div>
+              <div className="border border-blue-500 bg-slate-800 rounded-md p-6 mx-9">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                minima vero officiis ratione nam, provident ab dignissimos et
+                minus necessitatibus! Accusamus voluptatum in neque, eum est,
+                sunt, et fugiat ullam ducimus cumque unde voluptas consequatur!
+              </div>
             </div>
 
-            <button class="btn btn-active mx-8 bg-indigo-900 border-indigo-700 mt-9 h-[4rem] text-white text-md">
+            <button className="custom-btn btn btn-active mx-8 bg-blue-950 border-blue-500 mt-9 h-[4rem] text-slate-300  text-lg rounded-xl hover:bg-blue-800 hover:border-blue-400">
               E14 Limehouse
             </button>
 
-            <button class="btn btn-active mx-8 bg-indigo-900 border-indigo-700 mt-9 h-[4rem] text-white text-md">
+            <button className="btn btn-active mx-8 bg-blue-950 border-blue-600 mt-4 h-[4rem] text-slate-300 text-lg rounded-xl hover:bg-blue-800 hover:border-blue-400">
               E1 Mile End
             </button>
 
-            <button class="btn btn-active mx-8 bg-indigo-900 border-indigo-700 my-9 h-[4rem] text-white text-md">
-              E2 Bethnal Green
+            <button className="btn btn-active mx-8 bg-blue-950 border-blue-700 my-4 h-[4rem] text-slate-300 text-lg rounded-xl hover:bg-blue-800 hover:border-blue-400">
+              E2 Bethnal Blue
             </button>
-
-            <li>{/* <a>Sidebar Item 2</a> */}</li>
           </div>
         </div>
       </div>
